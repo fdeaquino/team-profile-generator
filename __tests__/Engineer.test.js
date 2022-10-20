7e6d2b109cde4cb4c7b2883c7a8319d934205313
FDeaquino <fidel.deaquino@gmail.com>
2022-10-20 10:16:30 -0500

    removed random test files

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,13 +1,13 @@
 const Engineer = require ('../lib/Engineer.js');
 
 test("Get the Engineer's role", () => {
-    const engineer = new Engineer('Chavo', '1', 'chavo@email.com', 'chavo8');
+    const engineer = new Engineer('Chavo', 11, 'chavo@email.com', 'chavo8');
     
     expect(engineer.getRole()).toBe('Engineer');
 });
 
 test("Get the Engineer's github username", () => {
-    const engineer = new Engineer('Chavo', '1', 'chavo@email.com', 'chavo8');
+    const engineer = new Engineer('Chavo', 11, 'chavo@email.com', 'chavo8');
     const expectedGithub = expect.any(String);
     expect(engineer.getGithub()).toEqual(expectedGithub);
 });
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,12 +1,12 @@
 const Manager = require ('../lib/Manager.js');
 
 test("Get the Manager's Role", () => {
-    const manager = new Manager('Chavo', '1', 'chavo@email.com', 100);
+    const manager = new Manager('Chavo', 11, 'chavo@email.com', 100);
     expect(manager.getRole()).toBe('Manager');
 });
 
 test("Get the manager's office number", () => {
-    const manager = new Manager('Chavo', '1', 'chavo@email.com', 100);
+    const manager = new Manager('Chavo', 11, 'chavo@email.com', 100);
     const expectedOfficeNum = expect.any(Number);
     expect(manager.getOfficeNum()).toEqual(expectedOfficeNum);
 });
